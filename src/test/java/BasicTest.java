236fbe02f5f55752135bea071fb9601c959cca89
kristjan <kristjan.lohmus@gmail.com>
2019-03-26 17:17:51 +0200

    8 Tests added, 2 more to go!

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,9 +1,12 @@
+import junit.framework.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -13,49 +16,140 @@ public class BasicTest extends TestHelper {
     private String username = "";
     private String password = "";
 
+    //Test if items do get added to shopping cart
     @Test
-    public void titleExistsTest(){
-        String expectedTitle = "ST Online Store";
-        String actualTitle = driver.getTitle();
-
-        assertEquals(expectedTitle, actualTitle);
-    }
-
-
-    /*
-    In class Exercise
-
-    Fill in loginLogoutTest() and login mehtod in TestHelper, so that the test passes correctly.
-
-     */
-    // @Test
-    public void loginLogoutTest(){
-
-        login(username, password);
-
-        // assert that correct page appeared
-        // WebElement adminHeader = driver.findElement...
-        // ...
-
-        logout();
-    }
-
-    /*
-    In class Exercise
-
-     Write a test case, where you make sure, that one can’t log in with a false password
-
-     */
-    // @Test
-    public void loginFalsePassword() {
-
-    }
-    @Test
-    public void addItemToShoppingCart(){
+    public void addAllItemsToShoppingCart() throws InterruptedException {
 
         List<WebElement> entries = getEntries();
-        System.out.println(entries);
 
+        assertEquals(0, getCartEntries().size());
+
+        addItemsToShoppingCart(entries);
+        TimeUnit.SECONDS.sleep(2);
+        List<WebElement> cartEntries = getCartEntries();
+        assertEquals(entries.size(), cartEntries.size());
     }
 
+    //Test if the items added to the shopping cart match in names and in order
+    @Test
+    public void checkShoppingCartNamesMatchCatalogue() throws InterruptedException {
+        List<WebElement> entries = getEntries();
+        addItemsToShoppingCart(entries);
+        TimeUnit.SECONDS.sleep(2);
+        ArrayList<String> names = new ArrayList<String>();
+        List<WebElement> cartEntries = getCartEntries();
+        for (int i = 0; i < cartEntries.size(); i++) {
+            String cartEntry = getShoppingCartItemTitle(cartEntries.get(i));
+            names.add(cartEntry);
+
+        }
+        for (int i = 0; i < entries.size(); i++) {
+            String shopEntry = getItemTitle(entries.get(i));
+            if (!names.contains(shopEntry))
+                assertEquals("Cart doesn't contain the item:", shopEntry, false);
+        }
+    }
+
+    @Test
+    public void checkShoppingCartPricesMatchCatalogue() throws InterruptedException {
+        List<WebElement> entries = getEntries();
+        addItemsToShoppingCart(entries);
+        TimeUnit.SECONDS.sleep(1);
+        List<WebElement> cartEntries = getCartEntries();
+        for (int i = 0; i < entries.size(); i++) {
+            String shopEntry = getItemPrice(entries.get(i));
+            String cartEntry = getShoppingCartItemPrice(cartEntries.get(i));
+            assertEquals("Prices from added items and shopping cart items were not identical.", shopEntry, cartEntry);
+        }
+    }
+
+    @Test
+    public void checkShoppingCartIncreaseAndDecrease() throws InterruptedException {
+        Integer increaseAmount = 4;
+        increaseQuantity(increaseAmount);
+        increaseAmount++;
+        String increaseAmountString = increaseAmount.toString() + "×";
+        String realAmount = getShoppingCartAmount();
+        assertEquals(increaseAmountString, realAmount);
+        decreaseQuantity(increaseAmount);
+        String originalAmount = "1" + "×";
+        realAmount = getShoppingCartAmount();
+        assertEquals(originalAmount, realAmount);
+    }
+
+    @Test
+    public void deleteItemsOneByOne() throws InterruptedException {
+        List<WebElement> entries = getEntries();
+        addItemsToShoppingCart(entries);
+        TimeUnit.SECONDS.sleep(1);
+        assertEquals(getCartEntries().size(), entries.size());
+        for (int i = 0; i < entries.size(); i++) {
+            removeFirstItem();
+        }
+        assertEquals(0, getCartEntries().size());
+    }
+
+    @Test
+    public void emptyCart() throws InterruptedException {
+        List<WebElement> entries = getEntries();
+        addItemsToShoppingCart(entries);
+        TimeUnit.SECONDS.sleep(1);
+        assertEquals(getCartEntries().size(), entries.size());
+        emptyShoppingCart();
+        assertEquals(0, getCartEntries().size());
+    }
+
+    @Test
+    public void categoryTesting() throws InterruptedException {
+
+        HashMap<String, ArrayList<String>> hashMap = getCategories();
+        switchToSunglassesTab();
+        TimeUnit.SECONDS.sleep(1);
+
+        ArrayList<String> sunglassTitles = hashMap.get("Sunglasses");
+        ArrayList<String> realTitles = getTitles();
+        assertEquals(sunglassTitles, realTitles);
+
+
+        switchToBooksTab();
+        TimeUnit.SECONDS.sleep(1);
+        ArrayList<String> booksTitles = hashMap.get("Books");
+        realTitles = getTitles();
+        Collections.reverse(realTitles);
+        assertEquals(booksTitles, realTitles);
+
+
+        switchToOtherTab();
+        TimeUnit.SECONDS.sleep(1);
+        ArrayList<String> otherTitles = hashMap.get("Other");
+        realTitles = getTitles();
+        Collections.reverse(realTitles);
+        assertEquals(otherTitles, realTitles);
+    }
+
+    @Test
+    public void testSearching() throws InterruptedException {
+        HashMap<String, String> titleSearches = titleSearches();
+        ArrayList<String> titles = getTitles();
+        WebElement searchBox = getSearchBox();
+        for (String title : titles) {
+
+            TimeUnit.SECONDS.sleep(1);
+
+            searchBox.sendKeys(titleSearches.get(title));
+            TimeUnit.SECONDS.sleep(3);
+
+            ArrayList<String> results = getTitles();
+            assertEquals("The results did not contain the nessecary result", true, results.contains(title));
+            searchBox.clear();
+        }
+    }
+
+    @Test
+    public void testCheckout() throws InterruptedException {
+        addItemsToShoppingCart(getEntries());
+        increaseQuantity(2);
+        clickCheckoutButton();
+
+    }
 }
--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -1,15 +1,16 @@
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -17,15 +18,15 @@ public class TestHelper {
 
     static WebDriver driver;
     final int waitForResposeTime = 4;
-	
-	// here write a link to your admin website (e.g. http://my-app.herokuapp.com/admin)
+
+    // here write a link to your admin website (e.g. http://my-app.herokuapp.com/admin)
     String baseUrlAdmin = "https://radiant-cove-46717.herokuapp.com/admin";
-	
-	// here write a link to your website (e.g. http://my-app.herokuapp.com/)
+
+    // here write a link to your website (e.g. http://my-app.herokuapp.com/)
     String baseUrl = "https://radiant-cove-46717.herokuapp.com/";
 
     @Before
-    public void setUp(){
+    public void setUp() {
 
         // if you use Chrome:
         System.setProperty("webdriver.chrome.driver", "/home/kristjan/Documents/chromedriver");
@@ -40,13 +41,13 @@ public class TestHelper {
 
     }
 
-    void goToPage(String page){
+    void goToPage(String page) {
         WebElement elem = driver.findElement(By.linkText(page));
         elem.click();
         waitForElementById(page);
     }
 
-    void waitForElementById(String id){
+    void waitForElementById(String id) {
         new WebDriverWait(driver, waitForResposeTime).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
     }
 
@@ -54,14 +55,13 @@ public class TestHelper {
         try {
             driver.findElement(by);
             return true;
-        }
-        catch (NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             return false;
         }
     }
 
 
-    void login(String username, String password){
+    void login(String username, String password) {
 
         driver.get(baseUrlAdmin);
 
@@ -71,13 +71,159 @@ public class TestHelper {
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.xpath("//input[@value='Login']")).click();
     }
-    List<WebElement> getEntries(){
+
+    List<WebElement> getEntries() {
         By by = By.className("entry");
-        List<WebElement> entries =  driver.findElements(by);
+        List<WebElement> entries = driver.findElements(by);
         return entries;
     }
 
-    void logout(){
+    ArrayList<String> getTitles() {
+        ArrayList<WebElement> sunglassItems = (ArrayList<WebElement>) getEntries();
+        ArrayList<String> realTitles = new ArrayList<>();
+        for (WebElement sunglassItem : sunglassItems) {
+            realTitles.add(getItemTitle(sunglassItem));
+        }
+        return realTitles;
+    }
+
+
+    HashMap<String, String> titleSearches() {
+        ArrayList<String> titles = getTitles();
+        HashMap<String, String> searches = new HashMap<>();
+        for (String title : titles) {
+            if (title.length() > 5) {
+                searches.put(title, title.substring(0, 5));
+            } else {
+                searches.put(title, title);
+            }
+        }
+        return searches;
+    }
+
+    List<WebElement> getCartEntries() {
+        return driver.findElements(By.className("cart_row"));
+    }
+
+    void addItemsToShoppingCart(List<WebElement> entries) {
+        for (WebElement element :
+                entries) {
+            element.findElement(new By.ByXPath(".//div[2]/form/input[1]")).click();
+        }
+    }
+
+    void addItemToShoppingCart(WebElement entry) {
+        entry.findElement(new By.ByXPath(".//div[2]/form/input[1]")).click();
+    }
+
+    String getItemTitle(WebElement item) {
+        return item.findElement(new By.ByXPath(".//h3//a")).getText();
+    }
+
+    String getShoppingCartItemTitle(WebElement item) {
+        return item.findElement(new By.ByXPath(".//td[2]")).getText();
+    }
+
+    String getItemPrice(WebElement item) {
+        return item.findElement(new By.ByClassName("price")).getText();
+    }
+
+    String getShoppingCartItemPrice(WebElement item) {
+        return item.findElement(new By.ByClassName("item_price")).getText();
+    }
+
+    void increaseQuantity(Integer increaseAmount) throws InterruptedException {
+        addItemToShoppingCart(getEntries().get(0));
+        for (int i = 0; i < increaseAmount; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            List<WebElement> cartEntries = getCartEntries();
+            WebElement firstEntry = cartEntries.get(0);
+            WebElement increaseButton = firstEntry.findElement(new By.ByXPath(".//td[5]"));
+            increaseButton.click();
+        }
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    void decreaseQuantity(Integer decreaseAmount) throws InterruptedException {
+        for (int i = 0; i < decreaseAmount - 1; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            List<WebElement> cartEntries = getCartEntries();
+            WebElement firstEntry = cartEntries.get(0);
+            WebElement decreaseButton = firstEntry.findElement(new By.ByXPath(".//td[4]"));
+            decreaseButton.click();
+        }
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    public void removeFirstItem() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+        List<WebElement> cartEntries = getCartEntries();
+        WebElement firstEntry = cartEntries.get(0);
+        WebElement decreaseButton = firstEntry.findElement(new By.ByXPath(".//td[6]//a"));
+        decreaseButton.click();
+    }
+
+    String getShoppingCartAmount() {
+        return driver.findElement(new By.ByXPath("//*[@id=\"cart\"]/table/tbody/tr[1]/td[1]")).getText();
+    }
+
+    WebElement getSearchBox() {
+        return driver.findElement(new By.ById("search_input"));
+    }
+
+    void emptyShoppingCart() {
+        driver.findElement(new By.ByXPath("//*[@id=\"cart\"]/form[1]/input[2]")).click();
+    }
+
+    HashMap<String, ArrayList<String>> getCategories() {
+        HashMap<String, ArrayList<String>> returnable = new HashMap<String, ArrayList<String>>();
+        returnable.put("Books", new ArrayList<>());
+        returnable.put("Sunglasses", new ArrayList<>());
+        returnable.put("Other", new ArrayList<>());
+        List<WebElement> elements = getEntries();
+        for (WebElement element : elements) {
+            String title = getItemTitle(element);
+            String category = element.findElement(new By.ById("category")).getText().substring(9).trim();
+            if (!category.equals("Books") && !category.equals("Sunglasses")) {
+                category = "Other";
+            }
+            returnable.get(category).add(title);
+        }
+        return returnable;
+    }
+
+    void clickCheckoutButton() {
+        driver.findElement(new By.ByXPath("//*[@id=\"checkout_button\"]/input")).click();
+    }
+
+    void switchToBooksTab() {
+        driver.findElement(new By.ByXPath("//body//div[2]//div[1]//ul//li[3]//a")).click();
+    }
+
+    void switchToSunglassesTab() {
+        driver.findElement(new By.ByXPath("//body//div[2]//div[1]//ul//li[2]//a")).click();
+    }
+
+    void switchToOtherTab() {
+        driver.findElement(new By.ByXPath("//body//div[2]//div[1]//ul//li[4]//a")).click();
+    }
+
+    void fillDetailsForm(String name, String address, String email, String payType) {
+        WebElement nameField = driver.findElement(By.id("order_name"));
+        nameField.sendKeys(name);
+        WebElement addressField = driver.findElement(By.id("order_address"));
+        addressField.sendKeys(address);
+        WebElement emailField = driver.findElement(By.id("order_email"));
+        emailField.sendKeys(email);
+        Select paymentField = (Select) driver.findElement(By.id("order_pay_type"));
+        paymentField.selectByValue(payType);
+    }
+
+    void clickPlaceOrderButton() {
+        driver.findElement(new By.ByXPath("//*[@id=\"place_order\"]/input")).click();
+    }
+
+    void logout() {
         WebElement logout = driver.findElement(By.linkText("Logout"));
         logout.click();
 
@@ -85,7 +231,7 @@ public class TestHelper {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
     }
 
